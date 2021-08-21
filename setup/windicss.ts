c28c80476e721cb4158d35eeb7b006142f0fc8d8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-21 18:03:05 -0400

    Add the fontFamily to the windicss.ts file.

--- setup/windicss.ts
+++ setup/windicss.ts
@@ -11,10 +11,11 @@ export default defineWindiSetup(() => ({
   theme: {
     extend: {
       // fonts can be replaced here, remember to update the web font links in `index.html`
+      // note that it does not actually seem to be needed to update the index.html file!
       fontFamily: {
-        sans: 'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
-        mono: '"Fira Code", monospace',
-      },
+          sans: '"IBM Plex Sans","Nunito Sans",ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+          mono: '"IBM Plex Mono", monospace',
+        },
     },
   },
 }))
